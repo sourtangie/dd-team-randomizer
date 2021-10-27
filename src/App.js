95eb5be91dac3a3ff94b40492c0bac51407b5cc3
sourtangie <molleetje@gmail.com>
2021-10-27 19:14:45 +0200

    v1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,113 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import { HeroContainer } from "./HeroContainer";
+import runaway from "./heroes/runaway.png";
+import graveRobber from "./heroes/Grave_Robber.webp";
+import hellion from "./heroes/Hellion.webp";
+import highwayman from "./heroes/Hwyman.webp";
+import jester from "./heroes/Jester.webp";
+import leper from "./heroes/Leper.webp";
+import manatarms from "./heroes/Man-At-Arms.webp";
+import occultist from "./heroes/Occultist.webp";
+
+import plagueDoctor from "./heroes/Plague_Doctor.webp";
 
 function App() {
+  const heroes = [
+    { name: "Grave Robber", id: 0, img: graveRobber },
+    { name: "Hellion", id: 1, img: hellion },
+    { name: "Highwayman", id: 2, img: highwayman },
+    { name: "Jester", id: 3, img: jester },
+    { name: "Leper", id: 4, img: leper },
+    { name: "Man-at-arms", id: 5, img: manatarms },
+    { name: "Occultist", id: 6, img: occultist },
+    { name: "Plague Doctor", id: 7, img: plagueDoctor },
+    { name: "Runaway", id: 8, img: runaway },
+  ];
+  const teamSize = 4;
+  const [selectedHeroes, setSelectedHeroes] = useState([]);
+  const [selectedHero, setSelectedHero] = useState([]);
+  const [secondHero, setSecondHero] = useState();
+  const [thirdHero, setThirdHero] = useState();
+  const [fourthHero, setFourthHero] = useState();
+
+  const generateTeam = () => {
+    let heroesAvailable = heroes;
+    let chosenIDs = [];
+    let newID;
+    for (let i = 0; i < 4; i++) {
+      newID = chooseHero();
+      while (chosenIDs.includes(newID)) {
+        newID = chooseHero();
+      }
+      chosenIDs.push(newID);
+    }
+    setSelectedHeroes([
+      heroes[chosenIDs[0]],
+      heroes[chosenIDs[1]],
+      heroes[chosenIDs[2]],
+      heroes[chosenIDs[3]],
+    ]);
+  };
+
+  const chooseHero = () => {
+    return Math.floor(Math.random() * 9);
+  };
+
+  if (selectedHeroes.length > 3) {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>Darkest Dungeon 2 Hero Selector</p>
+        </header>
+        <div className="page-container">
+          <div className="team-container">
+            <div className="hero-container">
+              <div className="moon">
+                <img className="hero-img" src={selectedHeroes[0].img}></img>
+                <p className="hero-name">{selectedHeroes[0].name}</p>
+              </div>
+            </div>
+            <div className="hero-container">
+              <div className="moon">
+                <img className="hero-img" src={selectedHeroes[1].img}></img>
+                <p className="hero-name">{selectedHeroes[1].name}</p>
+              </div>
+            </div>
+            <div className="hero-container">
+              <div className="moon">
+                <img className="hero-img" src={selectedHeroes[2].img}></img>
+                <p className="hero-name">{selectedHeroes[2].name}</p>
+              </div>
+            </div>
+            <div className="hero-container">
+              <div className="moon">
+                <img className="hero-img" src={selectedHeroes[3].img}></img>
+                <p className="hero-name">{selectedHeroes[3].name}</p>
+              </div>
+            </div>
+          </div>
+          <button className="generate-btn" onClick={() => generateTeam()}>
+            Pick new team
+          </button>
+        </div>
+      </div>
+    );
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Darkest Dungeon Hero Selector</p>
       </header>
+      <div className="team-container">
+        <div className="hero-container"></div>
+        <div className="hero-container"></div>
+        <div className="hero-container"></div>
+        <div className="hero-container"></div>
+        <button className="generate-btn" onClick={() => generateTeam()}>
+          Pick new team
+        </button>
+      </div>
     </div>
   );
 }
